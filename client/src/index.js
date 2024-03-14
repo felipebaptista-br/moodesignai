ad05ea4190e0ac18be6a3018487cc1d32147f086
Felipe Baptista <felipe.baptista06@gmail.com>
2024-03-14 16:39:51 -0300

    created a new project in Full React

---
+++ client/src/index.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+} from "react-router-dom";
+import Studio from "./pages/studio";
+
+import './global.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "/studio",
+    element: <Studio />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
